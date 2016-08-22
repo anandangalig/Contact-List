031719d86f21a98b156db6fcd98eb4ad77101594
Anand Angalig and Cornelius Range <cornelius@>
2016-08-22 10:11:06 -0400

    add skeleton files

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $("form#userInput").submit(function(event){
+    event.preventDefault();
+    $("ul").html("");
+    var input = parseInt($("#inputNumber").val());
+    var inputArray = [];
+
+    for (var index = 1; index <= input; index++) {
+      inputArray.push(index);
+    }
+
+
+    inputArray.forEach(function(currentInteger) {
+      if (currentInteger % 15 ===0) {
+        $("ul").append("<li>" + "PING-PONG!" + "</li>");
+        $("img").show();
+      } else if (currentInteger % 5 ===0) {
+        $("ul").append("<li>" + "PONG!" + "</li>");
+        $("img").show();
+      } else if (currentInteger % 3 ===0) {
+        $("ul").append("<li>" + "PING!" + "</li>");
+        $("img").show();
+      } else if (currentInteger % 3 !==0 || currentInteger % 5 !==0 || currentInteger % 15 !==0) {
+        $("ul").append("<li>" + currentInteger + "</li>");
+        $("img").show();
+      //isNaN(currentInteger) and other itirations of it- DID NOT WORK
+      } else {
+        $("ul").append("<li>" + "Please enter a number and try again!" + "</li>");
+      }
+    })
+  });
+});
