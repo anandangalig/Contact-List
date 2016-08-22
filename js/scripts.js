2055e6006226736391df6983711068a7f82df341
Anand Angalig and Cornelius Range <cornelius@>
2016-08-22 11:23:35 -0400

    create output section with names using .click and .last() and clear the input filed for cosmetics with .val

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,26 @@ function Contact(first, last) {
 }
 
 // User Interface Logic
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("Chris");
+    $("input#new-last-name").val("Hemsworth");
+  });
+});
